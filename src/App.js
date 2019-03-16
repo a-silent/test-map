29e05ac4d63b7750f49f36d17a7579cfcbf1e1ae
Silent <tykhyi.andrii@gmail.com>
2019-03-16 23:18:37 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+
+import "./App.css";
+import "leaflet/dist/leaflet.css";
+
+import L from 'leaflet';
+
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 class App extends Component {
   render() {
+    const position = [49.934771, 36.244521];
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Map center={position} zoom={18}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            bad location for life =)
+          </Popup>
+        </Marker>
+      </Map>
     );
   }
 }
+
 
 export default App;
