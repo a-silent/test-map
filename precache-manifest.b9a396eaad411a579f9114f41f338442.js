46e28a32b8dd5a33994a230ef2a5acf6ddfceb25
Silent <tykhyi.andrii@gmail.com>
2019-03-16 23:22:15 +0200

    Updates

---
+++ precache-manifest.b9a396eaad411a579f9114f41f338442.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e96ec3533ef0ac23cb2",
+    "url": "/test-map/static/css/main.a21e43ae.chunk.css"
+  },
+  {
+    "revision": "0e96ec3533ef0ac23cb2",
+    "url": "/test-map/static/js/main.d79486cf.chunk.js"
+  },
+  {
+    "revision": "e258bfe3d4832b129940",
+    "url": "/test-map/static/js/runtime~main.43522433.js"
+  },
+  {
+    "revision": "8e465cddeb85f9b002ea",
+    "url": "/test-map/static/css/2.b29359bc.chunk.css"
+  },
+  {
+    "revision": "8e465cddeb85f9b002ea",
+    "url": "/test-map/static/js/2.aaf4fc19.chunk.js"
+  },
+  {
+    "revision": "f8f28d56d58956e57d86b02c502837f7",
+    "url": "/test-map/index.html"
+  }
+];
